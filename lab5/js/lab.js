6fc0206d1d7e472dd5a627c82ad5b4f87a630c04
Emma LaPolt <elapolt02@gmail.com>
2022-10-17 12:31:59 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+/**
+ * Author:    Emma LaPolt
+ * Created:   10.17.2022
+ **/
+
+//Define variables
+ var make = "Ghia";
+ var model = "Volkswagen Karmann";
+ var color = "Light blue";
+ var year = 1968;
+
+ //Calculate
+ var age = 2022 - year;
+
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Age: " + age + "<br>");
