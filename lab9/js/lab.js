8c5ff2b1c5278b543371b44336f97af3e15e56c0
Emma LaPolt <elapolt02@gmail.com>
2022-10-31 11:42:58 -0700

    lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,18 @@
+/**
+*Author: Emma LaPolt
+*Created: 10/23/2022
+**/
+
+var outputEl = document.getElementById("output");
+
+var new1El = document.createElement("p");
+var new2El = document.createElement("p");
+
+new1El.innerHTML = "I'm doing this lab on Halloween!! :D";
+new2El.innerHTML = "I'm dressed as a character from the movie X. I'm very scary! >:)";
+
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+new1El.style.fontFamily="cursive";
+new2El.style.fontFamily="cursive";
